4e68228072b7d9c42a86f4beb197c3ea6d4c6ff8
pratik <pratiksingh260@gmail.com>
2023-02-22 12:46:07 +0530

    Filter function created in the Products Page

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -21,6 +21,10 @@ const Filters = () => {
     all_products,
   } = useFilterContext()
 
+  const categories = getUniqueValues(all_products, 'category')
+  const companies = getUniqueValues(all_products, 'company')
+  const colors = getUniqueValues(all_products, 'colors')
+
   return (
     <Wrapper>
       <div className='content'>
@@ -37,7 +41,116 @@ const Filters = () => {
             />
           </div>
           {/* end of search input */}
+          {/* category */}
+          <div className='form-control'>
+            <h5>Category</h5>
+            <div>
+              {categories.map((c, index) => {
+                return (
+                  <button
+                    key={index}
+                    onClick={updateFilter}
+                    type='button'
+                    name='category'
+                    className={`${
+                      category === c.toLowerCase() ? 'active' : null
+                    }`}
+                  >
+                    {c}
+                  </button>
+                )
+              })}
+            </div>
+          </div>
+          {/* end of category */}
+          {/* companies */}
+          <div className='form-control'>
+            <h5>Companies</h5>
+            <select
+              name='company'
+              value={company}
+              onChange={updateFilter}
+              className='company'
+            >
+              {companies.map((c, index) => {
+                return (
+                  <option key={index} value={c}>
+                    {c}
+                  </option>
+                )
+              })}
+            </select>
+          </div>
+          {/*  end of companies */}
+          {/* colors */}
+          <div className='form-control'>
+            <h5>colors</h5>
+            <div className='colors'>
+              {colors.map((c, index) => {
+                if (c === 'all') {
+                  return (
+                    <button
+                      key={index}
+                      name='color'
+                      onClick={updateFilter}
+                      className={`${
+                        color === 'all' ? 'all-btn active' : 'all-btn'
+                      }`}
+                      data-color='all'
+                    >
+                      all
+                    </button>
+                  )
+                }
+                return (
+                  <button
+                    key={index}
+                    name='color'
+                    style={{ background: c }}
+                    className={`${
+                      color === c ? 'color-btn active' : 'color-btn'
+                    }`}
+                    data-color={c}
+                    onClick={updateFilter}
+                  >
+                    {color === c ? <FaCheck /> : null}
+                  </button>
+                )
+              })}
+            </div>
+          </div>
+          {/* end of colors */}
+          {/* price */}
+          <div className='form-control'>
+            <h5>price</h5>
+            <p className='price'>{formatPrice(price)}</p>
+            <input
+              type='range'
+              name='price'
+              onChange={updateFilter}
+              min={min_price}
+              max={max_price}
+              value={price}
+            />
+          </div>
+          {/* end of price */}
+          {/* shipping */}
+          <div className='form-control shipping'>
+            <label htmlFor='shipping'>free shipping</label>
+            <input
+              type='checkbox'
+              name='shipping'
+              id='shipping'
+              onChange={updateFilter}
+              checked={shipping}
+            />
+          </div>
+          {/* end of shipping */}
         </form>
+        <button className='clear-btn' onClick={clearFilter}>
+          {' '}
+          clear filter
+        </button>
       </div>
     </Wrapper>
   )
