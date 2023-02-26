bc4d3f044b5b36d8b1d76579f6ad4a880a6b1796
pratik <pratiksingh260@gmail.com>
2023-02-27 00:38:20 +0530

    payment setup

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -2,8 +2,16 @@
 
 exports.handler = async function (event, context) {
   const { cart, shipping_fee, total_amount } = JSON.parse(event.body)
+  console.log(cart)
+
+  if (event.body) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(cart),
+    }
+  }
   return {
     statusCode: 200,
-    body: JSON.stringify(cart),
+    body: 'create payment intent',
   }
 }
